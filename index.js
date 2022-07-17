87080e9719b0c20cfc91166485f63336db244b88
Alberto Hinojosa <ahinojosa721@gmail.com>
2022-07-17 16:06:20 -0500

    commit completed javascript functions continued

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // code your solution here
+function saturdayFun(activity='roller-skate'){
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(event = 'go to the office') {
+    return (`This Monday, I will ${event}.`);
+}
+
+function wrapAdjective(string = '*'){
+    return function(name = 'special'){
+        return `You are ${string}${name}${string}!`
+    }
+}
